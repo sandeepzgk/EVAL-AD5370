2c325c0f96e33065e81ec5a66b6f9c2b79f50b91
Sandeep Zechariah George <sandeepzgk@gmail.com>
2020-04-20 17:18:15 -0700

    First Commit with DAC Class

---
+++ dac/DAC.cpp
@@ -0,0 +1,17 @@
+﻿#include "DAC.h"
+
+namespace AD537x {
+//void Simulation::updateVisuals() {}
+
+DAC::DAC() //Constructor
+{
+    
+}
+
+DAC::~DAC() //Destructor
+{
+
+}
+
+
+}  // namespace AD537x
---
+++ dac/DAC.h
@@ -0,0 +1,28 @@
+/*
+ *
+ */
+
+#ifndef DAC_H
+#define DAC_H
+
+#include <array>
+#include <map>
+
+
+namespace AD537x 
+{
+
+    class DAC 
+    {
+        private:
+            //List of private variables and functions
+        protected:
+            //List of protected variables and functions
+
+        public:
+            DAC();
+            ~DAC();
+
+    };
+}  // namespace AD537x
+#endif  // DAC_H
