b1200ea89f1cd34e91fd1d4cf34e9fd90a2286ea
Sandeep Zechariah George <sandeepzgk@gmail.com>
2020-06-02 13:26:05 -0700

    Few Updates to ensure performance... and Wiring Information

--- Test/spitest.cpp
+++ Test/spitest.cpp
@@ -18,6 +18,30 @@ sudo apt-get install gpio
 sudo apt-get install wiringpi
   
 ***/
+/******
+##############     Wiring Information         #############
+┌───────────────────────────────┬────────┬────────────────┐
+│      Raspberry Pi 3 (B+)      │        │  EVAL-AD5370   │
+├───┬───────┬──────────┬────────┤ Wire   ├────────┬───────┤
+│ # │ Pin#  │ Type     │ Name   │ Colour │ Name   │ Pin#  │
+├───┼───────┼──────────┼────────┼────────┼────────┼───────┤
+│ 1 │ 11    │ GPIO     │ LDAC   │ RED    │ /LDAC  │ 8     │
+│ 2 │ 13    │ GPIO     │ CLR    │ ORANGE │ /CLR   │ 10    │
+│ 3 │ 15    │ GPIO     │ BUSY   │ YELLOW │ /BUSY  │ 12    │
+│ 4 │ 7     │ GPIO     │ RESET  │ GREEN  │ /RESET │ 14    │
+│ 5 │ 19    │ SPI/MOSI │ MOSI   │ BLUE   │ DIN    │ 1     │
+│ 6 │ 21    │ SPI/MISO │ MISO   │ GREY   │ SDO    │ T6    │
+│ 7 │ 23    │ SPI/SCLK │ SCLK   │ PURPLE │ SCLK   │ 3     │
+│ 8 │ 34/39 │ Ground   │ GND    │ BLACK  │ DGND   │ 19/20 │
+│ 9 │ 24    │ SPI/CE0  │ SS/CE0 │ BROWN  │ /SYNC  │ 6     │
+└───┴───────┴──────────┴────────┴────────┴────────┴───────┘
+** T6 is a test point for EVAL-AD5370
+** Pin# for EVAL-AD5370 is from J3 Header
+** Pin# for Raspberry Pi 3 B+ model is physical pin number
+** The code uses WiringPi Pin numbering.
+** Connect Atleast two grounds for stable connection
+
+*******/
 #include <iostream>
 #include <errno.h>
 #include <wiringPiSPI.h>
@@ -52,7 +76,7 @@ int main()
 	// Configure the wiringPiSPI interface.
 	// CHANNEL insicates chip select,
 	// 500000 indicates bus speed.
-	fd = wiringPiSPISetupMode(CHANNEL, 500000,3);  //Returns the File Descriptor for SPI --- SPI mode 3, 2 ,1 works, SPI mode 0 DOES NOT WORK, I have not noticed any speed difference between 3 modes, need to read more about AD5370 and how this modes impact speed if it does or not.
+	fd = wiringPiSPISetupMode(CHANNEL, 500000,1);  //Returns the File Descriptor for SPI --- SPI mode 3, 2 ,1 works, SPI mode 0 DOES NOT WORK, I have not noticed any speed difference between 3 modes, need to read more about AD5370 and how this modes impact speed if it does or not.
 
 	cout << "wiringPiSPISetupMode Init result: " << fd << endl;
 
@@ -68,25 +92,26 @@ int main()
 	
 	//Set LDAC_pin HIGH for continious update write
 	digitalWrite(LDAC_pin, HIGH);
-
+	cout << "Pre Loop"<< endl;
 	for(int i=0;i<100000;i++)
 	{
 		//[201, 98, 32] (for +1 V) , 201, is channel 1, 200, is channel 0, 203, is channel 3
-		buffer[0] = 203;
+		buffer[0] = 201;
 		buffer[1] = 98;
 		buffer[2] = 32;
-		//if(digitalRead(BUSY_pin) == HIGH) //Ensuring the system (AD5370) is not busy so that we can write to the register, not important for one channel, but might be important for multi channel, if we remove this IF statement we get ~ 8kHz. See Prologue Note on SPEED
+		if(digitalRead(BUSY_pin) == HIGH) //Ensuring the system (AD5370) is not busy so that we can write to the register, not important for one channel, but might be important for multi channel, if we remove this IF statement we get ~ 8kHz. See Prologue Note on SPEED
 			wiringPiSPIDataRW(CHANNEL, buffer, 3); //The word has to be written simultaneously, the buffer is overwritten by the read activity (RW), after the write.
 
 		//usleep(1); //Sleep to stabilize the loop, so far we have achieved 7.7 kHz, but it flickers around 7.5 to 7.8 kHz, to stabilize the loop, the usleep timer is used, may be more precision timers may be required for the future.
 		
 		//[201, 72, 135]   (for -1 V)		
-		buffer[0] = 203;
+		buffer[0] = 201;
 		buffer[1] = 72;
 		buffer[2] = 135;
-		//if(digitalRead(BUSY_pin) == HIGH)
+		if(digitalRead(BUSY_pin) == HIGH)
 			wiringPiSPIDataRW(CHANNEL, buffer, 3);
 		
 		//usleep(1);
 	}
+	cout << "Post Loop"<< endl;
 }
